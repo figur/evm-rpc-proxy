82da5bc64cf9f683d762511ec5ff2853efa9d824
figur <annjtaylor2@proton.me>
2023-04-26 02:53:09 +0000

    initial commit of gpt3.5

--- src/main.rs
+++ src/main.rs
@@ -1,205 +1,206 @@
-use bytes::Bytes;
-use dotenv::dotenv;
-use http::Uri;
-use hyper::service::{make_service_fn};
-use hyper::{Body, Client, Request, Response, Server};
-use reqwest::Client as ReqwestClient;
-use serde_json::{json, Value};
+use reqwest::header::{HeaderMap, HeaderValue};
+use reqwest::RequestBuilder;
+use serde_json::{json, to_string};
+use std::collections::HashMap;
 use std::env;
-use std::fmt;
-use std::sync::Arc;
-use std::time::Duration;
-use tokio::sync::RwLock;
-use tokio::time;
-use hyper::body::to_bytes;
-
-#[derive(Debug)]
-pub enum ProxyError {
-    AllRpcServersFailed,
-    HyperError(hyper::Error),
-}
-
-impl fmt::Display for ProxyError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            ProxyError::AllRpcServersFailed => write!(f, "All RPC servers failed"),
-            ProxyError::HyperError(err) => write!(f, "Hyper error: {}", err),
-        }
-    }
-}
-
-impl std::error::Error for ProxyError {}
-
-impl From<hyper::Error> for ProxyError {
-    fn from(err: hyper::Error) -> ProxyError {
-        ProxyError::HyperError(err)
-    }
-}
-pub struct ClonableRequest {
-    pub req: Request<ClonableBody>,
-}
-
-pub struct ClonableBody(pub Arc<Bytes>);
-
-impl From<Body> for ClonableBody {
-    fn from(body: Body) -> Self {
-        let body_bytes = tokio::runtime::Handle::current()
-            .block_on(async { to_bytes(body).await })
-            .unwrap();
-        ClonableBody(Arc::new(body_bytes))
-    }
-}
-
-impl Clone for ClonableBody {
-    fn clone(&self) -> Self {
-        ClonableBody(self.0.clone())
-    }
-}
-
-impl Into<Body> for ClonableBody {
-    fn into(self) -> Body {
-        Body::from(self.0.as_ref().clone())
-    }
-}
-
-
-impl Clone for ClonableRequest {
-    fn clone(&self) -> Self {
-        let req = &self.req;
-        let mut req_builder = Request::builder()
-            .method(req.method().clone())
-            .uri(req.uri().clone())
-            .version(req.version());
-
-        for (name, value) in req.headers().iter() {
-            req_builder = req_builder.header(name, value);
-        }
-
-        let body = self.req.body().clone();
-        req_builder.body(body).unwrap().into()
-    }
-}
-
-impl From<Request<ClonableBody>> for ClonableRequest {
-    fn from(req: Request<ClonableBody>) -> Self {
-        ClonableRequest { req }
-    }
-}
-
-type RpcServer = (String, u64, u128);
-
-async fn update_rpc_stats(
-    rpc_servers: Arc<RwLock<Vec<RpcServer>>>,
-) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let client: ReqwestClient = ReqwestClient::new();
-    let mut interval: time::Interval = time::interval(Duration::from_secs(10));
-
-    loop {
-        interval.tick().await;
-
-        let rpc_servers_cloned: Vec<(String, u64, u128)> = rpc_servers.read().await.clone();
-        let mut updated_servers: Vec<(String, u64, u128)> = Vec::new();
-        for (url, _, _) in &rpc_servers_cloned {
-            let latency: u128 = match client.get(url.clone()).send().await {
-                Ok(resp) => resp.status().as_u16() as u128,
-                Err(_) => continue,
-            };
-
-            let latest_block: u64 = match client
-                .post(url.clone())
-                .json(&json!({
-                    "jsonrpc": "2.0",
-                    "id": 1,
-                    "method": "eth_blockNumber",
-                    "params": []
-                }))
-                .send()
-                .await?
-                .json::<Value>()
-                .await
-            {
-                Ok(json) => json["result"].as_str().unwrap()[2..]
-                    .parse::<u64>()
-                    .unwrap(),
-                Err(_) => continue,
-            };
-
-            updated_servers.push((url.clone(), latest_block, latency));
-        }
-
-        updated_servers.sort_by_key(|(_, block, latency)| (u64::MAX - block, *latency));
-        *rpc_servers.write().await = updated_servers;
-    }
-}
-
-async fn get_best_rpc(rpc_servers: Vec<RpcServer>) -> Option<RpcServer> {
-    rpc_servers.first().cloned()
-}
-
-async fn proxy_request(
-    clonable_req: &ClonableRequest,
-    rpc_servers: Arc<RwLock<Vec<RpcServer>>>,
-) -> Result<Response<Body>, ProxyError> {
-    let client: Client<hyper::client::HttpConnector> = Client::new();
-
-    for _ in 0..rpc_servers.read().await.len() {
-        if let Some((url, _, _)) = get_best_rpc(rpc_servers.read().await.clone()).await {
-            let uri: String = format!("http://{}{}", url, clonable_req.req.uri());
-            let uri: Uri = uri.parse::<Uri>().unwrap();
-
-            // Clone the request and create a new ClonableBody instance
-            let cloned_body = ClonableBody(Arc::clone(&clonable_req.req.body().0));
-            let cloned_req = Request::builder()
-                .method(clonable_req.req.method().clone())
-                .uri(uri)
-                .version(clonable_req.req.version())
-                .header(http::header::HOST, url.as_str())
-                .body(cloned_body.into())
-                .unwrap();
-
-            match client.request(cloned_req).await {
-                Ok(resp) => return Ok(resp),
-                Err(_) => continue,
-            };
-        } else {
-            return Err(ProxyError::AllRpcServersFailed);
-        }
-    }
-
-    Err(ProxyError::AllRpcServersFailed)
-}
+use std::error::Error;
+use std::io::Read;
+use std::net::{SocketAddr, TcpListener, TcpStream};
+use std::str::FromStr;
+use std::sync::{Arc, Mutex};
+use std::thread;
+use std::time::{Duration, Instant};
+use serde::de::{self};
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    dotenv().ok();
-    let addr = ([127, 0, 0, 1], env::var("PORT")?.parse::<u16>()?).into();
-    let server_urls: Vec<String> = env::var("RPC_SERVERS")?
+async fn main() -> Result<(), Box<dyn Error>> {
+    // Load environment variables from .env file
+    dotenv::dotenv().ok();
+    let rpc_servers =
+        env::var("RPC_SERVERS").unwrap_or_else(|_| "http://localhost:8545".to_string());
+    let mut rpc_server_list = rpc_servers
         .split(',')
-        .map(|s| s.trim().to_string())
-        .collect();
-    let rpc_servers: Arc<RwLock<Vec<(String, u64, u128)>>> = Arc::new(RwLock::new(Vec::new()));
-    for url in server_urls {
-        rpc_servers.write().await.push((url, 0, 0)); // Remove unwrap() call
-    }
+        .map(String::from)
+        .collect::<Vec<String>>();
+    let refresh_interval = env::var("REFRESH_INTERVAL")
+        .unwrap_or_else(|_| "30".to_string())
+        .parse::<u64>()
+        .unwrap_or(30);
 
-    tokio::spawn(update_rpc_stats(rpc_servers.clone()));
+    // Set up header for JSON-RPC requests
+    let mut headers = HeaderMap::new();
+    headers.insert("Content-Type", HeaderValue::from_static("application/json"));
 
-    let make_svc = make_service_fn(move |_conn| {
-        let rpc_servers = rpc_servers.clone();
-        async move {
-            let cloned_req = ClonableRequest(req.clone());
-            match proxy_request(&cloned_req, rpc_servers.clone()).await {
-                Ok(response) => Ok(response),
-                Err(_) => Ok(Response::builder().status(500).body(Body::empty()).unwrap()),
+    // Start the proxy server on port 8080
+    let addr = SocketAddr::from_str("0.0.0.0:8080")?;
+    let listener = TcpListener::bind(addr)?;
+
+    // Create shared state for RPC server info
+    let shared_state = Arc::new(Mutex::new(ServerState::new(rpc_server_list.clone())));
+
+    // Spawn a thread to periodically update the RPC server info
+    let shared_state_ref = shared_state.clone();
+    tokio::spawn(async move {
+        loop {
+            tokio::time::sleep(Duration::from_secs(refresh_interval)).await;
+
+            let mut new_server_list = Vec::new();
+            for rpc_server in rpc_server_list.iter() {
+                let json_request = json!({
+                    "jsonrpc": "2.0",
+                    "method": "eth_blockNumber",
+                    "params": [],
+                    "id": 1,
+                });
+
+                let resp = reqwest::Client::new()
+                    .post(rpc_server)
+                    .headers(headers.clone())
+                    .body(json_request.to_string())
+                    .send().await;
+
+                if let Ok(mut resp) = resp {
+                    let mut buffer = String::new();
+                    resp.read_to_string(&mut buffer)?;
+                    if let Ok(response) = serde_json::from_str::<JsonResponse>(&buffer) {
+                       
+                        if let Some(block_number) = response.result {
+                            let response_time = Instant::now().duration_since(response.sent_time);
+                            new_server_list.push(ServerInfo {
+                                rpc_server: rpc_server.clone(),
+                                block_number: block_number,
+                                response_time: response_time.as_millis() as u64,
+                            });
+                        }
+                    }
+                }
             }
+    
+            new_server_list.sort_by_key(|server| (server.response_time, server.block_number));
+            let mut state = shared_state_ref.lock().unwrap();
+            state.rpc_servers = new_server_list
+                .iter()
+                .map(|s| s.rpc_server.clone())
+                .collect();
+            state.best_server = new_server_list.first().map(|s| s.rpc_server.clone());
         }
     });
-
-    let server = Server::bind(&addr).serve(make_svc);
-
-    println!("Listening on http://{}", addr);
-
-    server.await?;
-
+    
+    // Start handling incoming requests
+    for stream in listener.incoming() {
+        let shared_state_ref = shared_state.clone();
+        thread::spawn(move || {
+            if let Ok(mut stream) = stream {
+                // Read the incoming request
+                let mut buffer = [0; 1024];
+                let mut request = String::new();
+                loop {
+                    match stream.read(&mut buffer) {
+                        Ok(0) => break,
+                        Ok(n) => request.push_str(&String::from_utf8_lossy(&buffer[..n])),
+                        Err(_) => break,
+                    }
+                }
+    
+                // Parse the incoming request and find the best RPC server to forward it to
+                let rpc_servers = shared_state_ref.lock().unwrap().rpc_servers.clone();
+                let best_server = shared_state_ref.lock().unwrap().best_server.clone();
+                let rpc_server =
+                    best_server.unwrap_or_else(|| rpc_servers.first().unwrap().clone());
+    
+                // Send the incoming request to the best RPC server
+                let response = reqwest::Client::new()
+                    .post(&rpc_server)
+                    .headers(headers)
+                    .body(request)
+                    .send();
+    
+                // Output logs based on the result of the request
+                match response {
+                    Ok(resp) => {
+                        println!("Received request, forwarded to RPC server: {}", rpc_server);
+                        if resp.status().is_client_error() || resp.status().is_server_error() {
+                            println!(
+                                "\x1b[31mRequest to RPC server {} failed: {}\x1b[0m",
+                                rpc_server,
+                                resp.status()
+                            );
+                        }
+                        let mut stream = stream.try_clone().unwrap();
+                        if let Err(_) = resp.copy_to(&mut stream) {}
+                    }
+                    Err(e) => {
+                        println!(
+                            "\x1b[31mRequest to RPC server {} failed: {}\x1b[0m",
+                            rpc_server, e
+                        );
+                    }
+                }
+            }
+        });
+    }
+    
     Ok(())
+}
+
+struct ServerInfo {
+rpc_server: String,
+block_number: String,
+response_time: u64,
+}
+
+struct ServerState {
+rpc_servers: Vec<String>,
+best_server: Option<String>,
+}
+
+impl ServerState {
+    fn new(rpc_servers: Vec<String>) -> Self {
+        ServerState {
+            rpc_servers: rpc_servers,
+            best_server: None,
+        }
+    }
+}
+
+#[derive(Debug, Deserialize)]
+struct JsonResponse {
+    jsonrpc: String,
+    result: Option<String>,
+    id: u64,
+    sent_time: String,
+}
+
+impl<'de> Deserialize<'de> for JsonResponse {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+        where
+        D: Deserializer<'de>,
+        {
+        #[derive(Deserialize)]
+        #[serde(rename_all = "camelCase")]
+        struct InnerJsonResponse {
+        jsonrpc: String,
+        result: Option<String>,
+        id: u64,
+        sent_time: String
+    }
+}
+
+let inner = InnerJsonResponse::deserialize(deserializer)?;
+    Ok(JsonResponse {
+        jsonrpc: inner.jsonrpc,
+        result: inner.result,
+        id: inner.id,
+        sent_time: Instant::from_str(&inner.sent_time).map_err(de::Error::custom)?,
+    }
+);
+
+fn deserialize_in_place<D>(deserializer: D, place: &mut Self) -> Result<(), D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        // Default implementation just delegates to `deserialize` impl.
+        *place = try!(Deserialize::deserialize(deserializer));
+        Ok(())
+    }
 }
